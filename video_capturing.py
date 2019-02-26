2e71e7e515235dcbec6739afe8c278c22a6eaf59
hien <hien@black.ai>
2019-02-26 15:33:14 +1100

    Initial commit

---
+++ video_capturing.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+cv2.namedWindow('Test')
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if ret == True:
+        #frame = cv2.flip(frame, 0)
+
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # write the flipped frame
+        out.write(frame)
+
+
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+
+
+# When everything done, release the capture
+cap.release()
+out.release()
+cv2.destroyAllWindows()
